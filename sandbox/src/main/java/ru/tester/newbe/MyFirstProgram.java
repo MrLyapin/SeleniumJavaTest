41535c97ab60ac865a349cfcaabc340062bd527e
MrLyapin <nick.v.lyapin@gmail.com>
2019-02-08 13:53:01 +0300

    Added TestNG

--- sandbox/src/main/java/ru/tester/newbe/MyFirstProgram.java
+++ sandbox/src/main/java/ru/tester/newbe/MyFirstProgram.java
@@ -2,27 +2,27 @@ package ru.tester.newbe;
 
 public  class MyFirstProgram {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		hello("user");
-		hello("world");
-
-
-		Square s = new Square(5);
-
-		System.out.println("Square area " + s.l + " = " + s.area());
-
-		Rectangle r = new Rectangle(4,5);
-
-		System.out.println("The area of the rectangle with sides " + r.a + " and " + r.b + " = " + r.area());
-	}
+    hello("user");
+    hello("world");
 
 
-	public static void hello(String somebody) {
+    Square s = new Square(5);
 
-		System.out.println("Hello "+ somebody +"!");
+    System.out.println("Square area " + s.l + " = " + s.area());
 
-	}
+    Rectangle r = new Rectangle(4,5);
+
+    System.out.println("The area of the rectangle with sides " + r.a + " and " + r.b + " = " + r.area());
+  }
+
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello "+ somebody +"!");
+
+  }
 
 }
 

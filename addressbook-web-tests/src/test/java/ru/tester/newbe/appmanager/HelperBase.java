75c6e0ce9164ea21b351fe5b653b4ee7dba5d272
Lyapin N.V <nick.v.lyapin@gmail.com>
2019-02-13 11:07:45 +0300

    Refactoring

--- addressbook-web-tests/src/test/java/ru/tester/newbe/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/tester/newbe/appmanager/HelperBase.java
@@ -1,9 +1,14 @@
 package ru.tester.newbe.appmanager;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 public class HelperBase {
+
+   private boolean acceptNextAlert = true;
    protected WebDriver driver;
 
    public HelperBase(WebDriver driver) {
@@ -19,4 +24,38 @@ public class HelperBase {
       driver.findElement(locator).clear();
       driver.findElement(locator).sendKeys(text);
    }
+
+   private boolean isElementPresent(By by) {
+      try {
+         driver.findElement(by);
+         return true;
+      } catch (NoSuchElementException e) {
+         return false;
+      }
+   }
+
+   private boolean isAlertPresent() {
+      try {
+         driver.switchTo().alert();
+         return true;
+      } catch (NoAlertPresentException e) {
+         return false;
+      }
+   }
+
+   private String closeAlertAndGetItsText() {
+      try {
+         Alert alert = driver.switchTo().alert();
+         String alertText = alert.getText();
+         if (acceptNextAlert) {
+            alert.accept();
+         } else {
+            alert.dismiss();
+         }
+         return alertText;
+      } finally {
+         acceptNextAlert = true;
+      }
+   }
+
 }
